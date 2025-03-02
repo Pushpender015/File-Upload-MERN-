ba07a428e2c269498a978c9090271c4acc58bb2b
Pushpender Singh <pushpender7291rajput@gmail.com>
2025-03-02 20:06:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cors({
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'X-Api-Version']
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // MIDDLEWARES
